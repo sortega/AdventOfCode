20a3d069e6e24697f89aa4820d5f416e438521da
Sebastian Ortega <sebastian.ortega@jobandtalent.com>
2016-01-30 00:23:50 +0100

    Day 5

---
+++ src/test/scala/advent/Day5Test.scala
@@ -0,0 +1,31 @@
+package advent
+
+import org.scalatest.{FlatSpec, ShouldMatchers}
+
+class Day5Test extends FlatSpec with ShouldMatchers {
+
+  "Part 1" should "consider nice strings with 3 vowels and a double letter" in {
+    Day5.part1("ugknbfddgicrmopn") shouldBe 1
+    Day5.part1("aaa") shouldBe 1
+  }
+
+  it should "consider naughty strings without doubled letters" in {
+    Day5.part1("jchzalrnumimnmhp") shouldBe 0
+  }
+
+  it should "consider naughty strings containing xy" in {
+    Day5.part1("haegwjzuvuyypxyu") shouldBe 0
+  }
+
+  it should "consider naughty strings containing less than 3 vowels" in {
+    Day5.part1("dvszwmarrgswjxmb") shouldBe 0
+  }
+
+  "Part 2" should "consider strings with repeated pairs and 3-palindromes nice" in {
+    Day5.part2("qjhvhtzxzqqjkmpb\nxxyxx") shouldBe 2
+  }
+
+  it should "consider strings without repeated pairs naughty" in {
+    Day5.part2("ieodomkazucvgmuy") shouldBe 0
+  }
+}
