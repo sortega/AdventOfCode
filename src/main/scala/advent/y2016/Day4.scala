a3d47084e9b384b9585cd0d151fc08460e4b8111
Sebastian Ortega <sebastian.ortega@jobandtalent.com>
2016-12-10 00:24:35 +0100

    Day 4

---
+++ src/main/scala/advent/y2016/Day4.scala
@@ -0,0 +1,43 @@
+package advent.y2016
+
+object Day4 {
+
+  val Alphabet = ('a' to 'z').toVector
+
+  case class Room(id: Int, name: String, checksum: String) {
+    def isValid: Boolean = checksum == generateChecksum
+
+    private def generateChecksum: String = {
+      val counts = name.groupBy(identity).mapValues(-_.length) - '-'
+      val mostFrequent = counts.toList.map(_.swap).sorted.take(5)
+      mostFrequent.map(_._2).mkString
+    }
+
+    def decrypt: String = name.map {
+      case '-' => ' '
+      case char => toChar(toIndex(char) + id)
+    }
+
+    def toIndex(c: Char): Int = Alphabet.indexOf(c)
+    def toChar(i: Int): Char = Alphabet(i % Alphabet.length)
+  }
+
+  object Room {
+    private val Pattern = """([-a-z]*?)-(\d+)\[([a-z]{5})\]""".r
+
+    def parseAll(input: String): Seq[Room] = input.split("\n").map {
+      case Pattern(name, id, checksum) => Room(id.toInt, name, checksum)
+    }
+  }
+
+  def part1(input: String): Int = Room.parseAll(input).filter(_.isValid).map(_.id).sum
+
+  def part2(input: String): Int =
+    Room.parseAll(input).find(_.decrypt == "northpole object storage").get.id
+
+  def main(args: Array[String]): Unit = {
+    val input = Source.fromInputStream(getClass.getResourceAsStream("day4.input")).mkString
+    println("Part 1 result: " + part1(input))
+    println("Part 2 result: " + part2(input))
+  }
+}
---
+++ src/test/scala/advent/y2016/Day4Test.scala
@@ -0,0 +1,25 @@
+package advent.y2016
+
+import org.scalatest.{FlatSpec, ShouldMatchers}
+
+class Day4Test extends FlatSpec with ShouldMatchers {
+
+  "Part 1" should "return the sum of valid room ids" in {
+    Day4.part1(
+      """aaaaa-bbb-z-y-x-123[abxyz]
+        |a-b-c-d-e-f-g-h-987[abcde]
+        |not-a-real-room-404[oarel]
+        |totally-real-room-200[decoy]
+        |""".stripMargin) shouldBe (123 + 404 + 987)
+  }
+
+  "Part 2" should "return the id of the encrypted 'North Pole objects'" in {
+    Day4.part2(
+      """aaaaa-bbb-z-y-x-123[abxyz]
+        |a-b-c-d-e-f-g-h-987[abcde]
+        |northpole-object-storage-0[oebce]
+        |not-a-real-room-404[oarel]
+        |totally-real-room-200[decoy]
+        |""".stripMargin) shouldBe 0
+  }
+}
