d956c780db083303f95d4c39de8d1e47db5d0e1b
Sebastian Ortega <sebastian.ortega@jobandtalent.com>
2016-12-10 12:14:32 +0100

    Day6

---
+++ src/main/scala/advent/y2016/Day6.scala
@@ -0,0 +1,29 @@
+package advent.y2016
+
+import scala.io.Source
+
+object Day6 {
+
+  def part1(input: String): String =
+    columnsOf(input).map(mostFrequent).mkString
+
+  private def mostFrequent(letters: List[Char]): Char =
+    frequencies(letters).toList.sortBy(-_._2).head._1
+
+  private def leastFrequent(letters: List[Char]): Char =
+    frequencies(letters).toList.sortBy(_._2).head._1
+
+  private def frequencies(letters: List[Char]) =
+    letters.groupBy(identity).mapValues(_.size)
+
+  private def columnsOf(input: String) =
+    input.lines.map(_.toList).toList.transpose
+
+  def part2(input: String): String = columnsOf(input).map(leastFrequent).mkString
+
+  def main(args: Array[String]): Unit = {
+    val input = Source.fromInputStream(getClass.getResourceAsStream("day6.input")).mkString
+    println("Part 1 result: " + part1(input))
+    println("Part 2 result: " + part2(input))
+  }
+}
---
+++ src/test/scala/advent/y2016/Day6Test.scala
@@ -0,0 +1,34 @@
+package advent.y2016
+
+import org.scalatest.{FlatSpec, ShouldMatchers}
+
+class Day6Test extends FlatSpec with ShouldMatchers {
+
+  private val sampleInput =
+    """eedadn
+      |drvtee
+      |eandsr
+      |raavrd
+      |atevrs
+      |tsrnev
+      |sdttsa
+      |rasrtv
+      |nssdts
+      |ntnada
+      |svetve
+      |tesnvt
+      |vntsnd
+      |vrdear
+      |dvrsen
+      |enarar
+      |""".stripMargin
+
+  "Part 1" should "take the most common letter per column" in {
+    Day6.part1("") shouldBe empty
+    Day6.part1(sampleInput) shouldBe "easter"
+  }
+
+  "Part 2" should "take the least common letter per column" in {
+    Day6.part2(sampleInput) shouldBe "advent"
+  }
+}
