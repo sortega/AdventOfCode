9de28688a64864c867656aed4a5e6ffc6f637cd1
Sebastian Ortega <sebastian.ortega@jobandtalent.com>
2016-12-15 22:49:11 +0100

    Day 15

---
+++ src/test/scala/advent/y2016/Day15Test.scala
@@ -0,0 +1,15 @@
+package advent.y2016
+
+import advent.y2016.Day15.Disc
+import org.scalatest.{FlatSpec, ShouldMatchers}
+
+class Day15Test extends FlatSpec with ShouldMatchers {
+
+  "Day 15" should "find the first time discs are aligned" in {
+    val discs = Seq(
+      Disc(1, positions = 5, startsAt = 4),
+      Disc(2, positions = 2, startsAt = 1)
+    )
+    Day15.apply(discs) shouldBe 5
+  }
+}
