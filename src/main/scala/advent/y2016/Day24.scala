5ac1b32cdaf3557c416e26dcf8b6403b9168502e
Sebastian Ortega <sebastian.ortega@jobandtalent.com>
2016-12-24 15:18:10 +0100

    Day 24

---
+++ src/main/scala/advent/y2016/Day24.scala
@@ -0,0 +1,72 @@
+package advent.y2016
+
+object Day24 {
+
+  type Route = Vector[Int]
+
+  case class Maze(spots: Map[Int, Point], openSpaces: Set[Point]) {
+    def distance(from: Point, to: Point): Int = {
+      val path = new AStar[Point](distance = (_, _) => 1,
+                                  neighbors = _.adjacent.intersect(openSpaces),
+                                  heuristic = point => (point - to).norm1).search(from, _ == to)
+      path.getOrElse(throw new Error(s"No path from $from to $to")).size - 1
+    }
+
+    def spotDistances: Map[(Int, Int), Int] =
+      for {
+        (from, fromPos) <- spots
+        (to, toPos) <- spots if to <= from
+        key <- Seq((from, to), (to, from))
+      } yield key -> distance(fromPos, toPos)
+  }
+
+  object Maze {
+    def parse(input: String): Maze = {
+      val charMap = (for {
+        (line, y) <- input.lines.zipWithIndex
+        (char, x) <- line.zipWithIndex
+      } yield Point(x, y) -> char).toMap
+
+      val spots = charMap.collect {
+        case (point, number) if ('0' to '9') contains number =>
+          number.toString.toInt -> point
+      }
+
+      val openSpaces = charMap.collect {
+        case (point, char) if char != '#' && !char.isSpaceChar => point
+      }.toSet
+
+      Maze(spots, openSpaces)
+    }
+  }
+
+  def shortestRouteDistance(distances: Map[(Int, Int), Int], routes: Int => Seq[Route]): Int = {
+    def totalDistance(route: Route): Int =
+      route
+        .sliding(size = 2, step = 1)
+        .map {
+          case Vector(from, to) => distances(from, to)
+        }
+        .sum
+
+    val spotsNum = distances.keys.map(_._1).max
+    routes(spotsNum).map(totalDistance).min
+  }
+
+  private def openRoutes(n: Int): Seq[Vector[Int]] =
+    (1 to n).toVector.permutations.map(route => 0 +: route).toSeq
+
+  private def closedRoutes(n: Int): Seq[Vector[Int]] = openRoutes(n).map(_ :+ 0)
+
+  def part1(input: String): Int =
+    shortestRouteDistance(Maze.parse(input).spotDistances, openRoutes)
+
+  def part2(input: String): Int =
+    shortestRouteDistance(Maze.parse(input).spotDistances, closedRoutes)
+
+  def main(args: Array[String]): Unit = {
+    val input = dailyResource(24).getLines().mkString("\n")
+    timed(println("Part 1 result: " + part1(input)))
+    timed(println("Part 2 result: " + part2(input)))
+  }
+}
---
+++ src/test/scala/advent/y2016/Day24Test.scala
@@ -0,0 +1,79 @@
+package advent.y2016
+
+import advent.y2016.Day24._
+import org.scalatest.{FlatSpec, ShouldMatchers}
+
+class Day24Test extends FlatSpec with ShouldMatchers {
+
+  private val sampleMaze = Maze.parse("""###########
+                                        |#0.1.....2#
+                                        |#.#######.#
+                                        |#4.......3#
+                                        |###########
+                                        |""".stripMargin)
+
+  "A maze" should "be parsed" in {
+    sampleMaze shouldBe Maze(
+      spots = Map(0 -> Point(1, 1),
+                  1 -> Point(3, 1),
+                  2 -> Point(9, 1),
+                  3 -> Point(9, 3),
+                  4 -> Point(1, 3)),
+      openSpaces = (1 to 9).toSet
+          .flatMap((x: Int) => Set(Point(x, 1), Point(x, 3))) ++ Set(Point(1, 2), Point(9, 2))
+    )
+  }
+
+  it should "compute distance between two points" in {
+    sampleMaze.distance(Point(1, 1), Point(4, 3)) shouldBe 5
+  }
+
+  it should "provide the distance map for spots" in {
+    sampleMaze.spotDistances shouldBe Map(
+      (0, 2) -> 8,
+      (0, 0) -> 0,
+      (4, 0) -> 2,
+      (3, 4) -> 8,
+      (3, 1) -> 8,
+      (4, 1) -> 4,
+      (2, 0) -> 8,
+      (0, 3) -> 10,
+      (4, 4) -> 0,
+      (3, 0) -> 10,
+      (1, 1) -> 0,
+      (1, 4) -> 4,
+      (0, 4) -> 2,
+      (3, 2) -> 2,
+      (1, 3) -> 8,
+      (2, 2) -> 0,
+      (4, 2) -> 10,
+      (2, 4) -> 10,
+      (0, 1) -> 2,
+      (3, 3) -> 0,
+      (2, 3) -> 2,
+      (1, 2) -> 6,
+      (2, 1) -> 6,
+      (4, 3) -> 8,
+      (1, 0) -> 2
+    )
+
+  }
+
+  "Part 1" should "get the shortest tourné" in {
+    Day24.part1("""###########
+                  |#0.1.....2#
+                  |#.#######.#
+                  |#4.......3#
+                  |###########
+                  |""".stripMargin) shouldBe 14
+  }
+
+  "Part 2" should "get the shortest tourné coming back to the starting point" in {
+    Day24.part2("""###########
+                  |#0.1.....2#
+                  |#.#######.#
+                  |#4.......3#
+                  |###########
+                  |""".stripMargin) shouldBe 20
+  }
+}
