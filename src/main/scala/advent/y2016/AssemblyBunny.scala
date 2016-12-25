6b21d83ef097a9ee9a4fd8732d4506cd35faef72
Sebastian Ortega <sebastian.ortega@jobandtalent.com>
2016-12-25 18:46:23 +0100

    Day 25

--- src/main/scala/advent/y2016/AssemblyBunny.scala
+++ src/main/scala/advent/y2016/AssemblyBunny.scala
@@ -22,10 +22,11 @@ object AssemblyBunny {
   case class DEC(r: Register) extends Instruction
   case class JNZ(cond: Value, offset: Value) extends Instruction
   case class TGL(r: Register) extends Instruction
+  case class OUT(r: Register) extends Instruction
 
   object AssemblyGrammar extends RegexParsers {
     private def listing: Parser[Vector[Instruction]] = instruction.* ^^ { _.toVector }
-    private def instruction: Parser[Instruction] = cpy | inc | dec | jnz | tgl
+    private def instruction: Parser[Instruction] = cpy | inc | dec | jnz | tgl | out
     private def cpy = "cpy" ~> value ~ value ^^ {
       case from ~ to => CPY(from, to)
     }
@@ -39,15 +40,19 @@ object AssemblyBunny {
       case value ~ offset => JNZ(value, offset)
     }
     private def tgl = "tgl" ~> register ^^ TGL.apply
+    private def out = "out" ~> register ^^ OUT.apply
 
     def parse(input: String): Vector[Instruction] = parseAll(listing, input).get
   }
 
   case class Computer(listing: IndexedSeq[Instruction],
                       registers: Map[Register, Int] = Map.empty.withDefaultValue(0),
-                      programCounter: Int = 0) {
+                      programCounter: Int = 0,
+                      output: Vector[Int] = Vector.empty) {
     @tailrec
     final def run: Computer = if (halted) this else step.run
+
+    def states: Stream[Computer] = Stream.iterate(this)(_.step).takeWhile(!_.halted)
 
     def halted: Boolean = programCounter >= listing.size
 
@@ -57,7 +62,8 @@ object AssemblyBunny {
         case CPY(_, Immediate(_)) => updateProgramCounter()
         case INC(r) => update(r)(_ + 1).updateProgramCounter()
         case DEC(r) => update(r)(_ - 1).updateProgramCounter()
-        case JNZ(cond, offset) => updateProgramCounter(if (lookup(cond) != 0) lookup(offset) else 1)
+        case JNZ(cond, offset) =>
+          updateProgramCounter(if (lookup(cond) != 0) lookup(offset) else 1)
         case TGL(offsetRegister) =>
           val index = programCounter + registers(offsetRegister)
           if (listing.indices.contains(index)) {
@@ -65,12 +71,13 @@ object AssemblyBunny {
               case INC(r) => DEC(r)
               case DEC(r) => INC(r)
               case TGL(r) => INC(r)
+              case OUT(r) => INC(r)
               case JNZ(value, offset) => CPY(value, offset)
               case CPY(from, to) => JNZ(from, to)
             }
             copy(listing = listing.updated(index, newInstruction)).updateProgramCounter()
           } else updateProgramCounter()
-
+        case OUT(r) => copy(output = output :+ registers(r)).updateProgramCounter()
       }
 
     private def lookup(v: Value): Int = v match {
@@ -81,6 +88,8 @@ object AssemblyBunny {
     def updateProgramCounter(by: Int = 1): Computer = copy(programCounter = programCounter + by)
     def update(r: Register, v: Int): Computer = copy(registers = registers.updated(r, v))
     def update(r: Register)(f: Int => Int): Computer = update(r, f(registers(r)))
+
+    def clearOutput: Computer = copy(output = Vector.empty)
   }
 
 }
---
+++ src/main/scala/advent/y2016/Day25.scala
@@ -0,0 +1,37 @@
+package advent.y2016
+
+import scala.annotation.tailrec
+
+import advent.y2016.AssemblyBunny._
+
+object Day25 {
+
+  private def generatesClockSignal(listing: Vector[Instruction], initialA: Int): Boolean =
+    validCycle(Computer(listing).update(Register.A, initialA).states)
+
+  private def validCycle(states: Stream[Computer]): Boolean = {
+
+    @tailrec
+    def findCycleStart(unseen: Stream[(Computer, Int)], seen: Set[Computer] = Set.empty): Int =
+      unseen match {
+        case (next, index) #:: _ if seen.contains(next.clearOutput) => index
+        case (next, _) #:: others => findCycleStart(others, seen + next.clearOutput)
+      }
+
+    val output = states(findCycleStart(states.zipWithIndex) * 2).output
+    isClockSignalPrefix(output)
+  }
+
+  private def isClockSignalPrefix(signal: Vector[Int]) =
+    Stream.from(0).map(_ % 2).startsWith(signal)
+
+  def part1(input: String): Int = {
+    val listing = AssemblyGrammar.parse(input)
+    Stream.from(0).filter(i => generatesClockSignal(listing, i)).head
+  }
+
+  def main(args: Array[String]): Unit = {
+    val input = dailyResource(25).getLines().mkString("\n")
+    timed(println("Part 1 result: " + part1(input)))
+  }
+}
