ecc1812e239b961e8d07a9dd3ea10e1c038a440d
Sebastian Ortega <sebastian.ortega@jobandtalent.com>
2017-12-14 18:21:27 +0100

    Extract KnotHash to an object

--- src/main/scala/advent/y2017/Day10.scala
+++ src/main/scala/advent/y2017/Day10.scala
@@ -4,6 +4,24 @@ import advent.shared.Time.timed
 
 object Day10 {
 
+  object KnotHash {
+    def apply(input: String): String = toHex(denseHash(sparseHash(decodeAscii(input))))
+
+    private def decodeAscii(text: String) = text.map(_.toInt).toVector
+
+    private def sparseHash(seed: Vector[Int]): Vector[Int] =
+      Vector
+        .fill(64)(seed ++ Vector(17, 31, 73, 47, 23))
+        .flatten
+        .foldLeft(Cycle.withLength(256))(_.pinchAndTwist(_))
+        .toVector
+
+    private def denseHash(sparseHash: Vector[Int]): Vector[Int] =
+      sparseHash.grouped(size = 16).map(_.reduce(_ ^ _)).toVector
+
+    private def toHex(bytes: Vector[Int]): String = bytes.map(_.formatted("%02x")).mkString
+  }
+
   case class Cycle(elems: Vector[Int], offset: Int, skipSize: Int) {
     def pinchAndTwist(length: Int): Cycle = {
       val (toReverse, toKeep) = elems.splitAt(length)
@@ -33,21 +51,7 @@ object Day10 {
   def part1(input: Vector[Int], length: Int = 256): Int =
     input.foldLeft(Cycle.withLength(length))(_.pinchAndTwist(_)).toVector.take(2).product
 
-  def part2(input: String): String = toHex(denseHash(sparseHash(decodeAscii(input))))
-
-  private def decodeAscii(text: String) = text.map(_.toInt).toVector
-
-  private def sparseHash(seed: Vector[Int]): Vector[Int] =
-    Vector
-      .fill(64)(seed ++ Vector(17, 31, 73, 47, 23))
-      .flatten
-      .foldLeft(Cycle.withLength(256))(_.pinchAndTwist(_))
-      .toVector
-
-  private def denseHash(sparseHash: Vector[Int]): Vector[Int] =
-    sparseHash.grouped(size = 16).map(_.reduce(_ ^ _)).toVector
-
-  private def toHex(bytes: Vector[Int]): String = bytes.map(_.formatted("%02x")).mkString
+  def part2(input: String): String = KnotHash(input)
 
   def main(args: Array[String]): Unit = {
     val input = Vector(83, 0, 193, 1, 254, 237, 187, 40, 88, 27, 2, 255, 149, 29, 42, 100)
